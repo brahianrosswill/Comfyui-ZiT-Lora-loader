1c7098bf7d537fe78e0d76ad4c66e03cb5fb226a
capitan01R <hussainmoha95@gmail.com>
2026-03-07 03:27:26 -0500

    Update __init__.py v1.2.0

--- __init__.py
+++ __init__.py
@@ -1,13 +1,5 @@
-from .zimage_turbo_lora_loader import ZImageTurboLoraLoader
+from .zimage_turbo_lora_loader import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-NODE_CLASS_MAPPINGS = {
-    "ZImageTurboLoraLoader": ZImageTurboLoraLoader,
-}
+WEB_DIRECTORY = "./js"
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "ZImageTurboLoraLoader": "Z-Image Turbo LoRA Loader",
-}
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-
-print("\033[92m[Z-Image Turbo LoRA]\033[0m Loaded: Z-Image Turbo LoRA Loader")
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
